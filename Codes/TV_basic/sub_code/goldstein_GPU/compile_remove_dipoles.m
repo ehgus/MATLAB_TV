cc46e8ca380c892e8b67012e83c8ff3ace497865
Dohyeon Lee <dleh428@kaist.ac.kr>
2023-12-14 16:23:47 +0900

    unwrapping algorithm

--- Codes/TV_basic/sub_code/goldstein_GPU/compile_remove_dipoles.m
+++ Codes/TV_basic/sub_code/goldstein_GPU/compile_remove_dipoles.m
@@ -1,3 +1,3 @@
 clc;
-cd('D:\Projects_\_BORN_CONVERGENT\_multiple_scattering_mshh_v2\sub_code\goldstein_GPU');
+cd(fileparts(matlab.desktop.editor.getActiveFilename));
 mexcuda -output remove_dipoles_gpu remove_dipoles_cuda.cu remove_dipoles_mex.cpp
--- Codes/TV_basic/sub_code/goldstein_GPU/compile_unwrapp_phase_goldstein.m
+++ Codes/TV_basic/sub_code/goldstein_GPU/compile_unwrapp_phase_goldstein.m
@@ -1,3 +1,3 @@
 clc;
-cd('D:\Projects_\_BORN_CONVERGENT\_multiple_scattering_mshh_v2\sub_code\goldstein_GPU');
+cd(fileparts(matlab.desktop.editor.getActiveFilename));
 mexcuda -output unwrapp_phase_goldstein_gpu unwrapp_phase_goldstein_cuda.cu unwrapp_phase_goldstein_mex.cpp
